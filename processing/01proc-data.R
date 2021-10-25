45c57e1f4b06507e6a1ad964eb4595e6dadfc587
Andreas-Lafferte <andreas.lafferte.tamayo@gmail.com>
2021-10-24 22:28:30 -0300

    Update changes

--- processing/01proc-data.R
+++ processing/01proc-data.R
@@ -5,6 +5,7 @@
 if (!require("pacman")) install.packages("pacman")
 
 pacman::p_load(tidyverse,
+               dplyr,
                lubridate,
                stringr, 
                sjlabelled, 
@@ -16,11 +17,11 @@ pacman::p_load(tidyverse,
                haven,
                stargazer,
                ggpubr,
-               psych)
+               psych,
+               magrittr)
 options(scipen=999)
 
 # 2. Data ----
-
 issp99 <- read_dta("../input/data/ISSP1999.dta")
 sapply(issp99, class)
 names(issp99)
@@ -28,7 +29,6 @@ names(issp99)
 # 3. Processing ----
 
 ## Select 
-
 issp99 <- issp99 %>% select(v1,
                             v2,
                             v3,
@@ -40,6 +40,8 @@ issp99 <- issp99 %>% select(v1,
                             nemploy,
                             wrkgovt, 
                             wrksup,
+                            educyrs,
+                            degree,
                             isco88_4,
                             selfemp,
                             v41,
@@ -58,13 +60,17 @@ issp99$YEAR <- sjlabelled::set_label(issp99$YEAR, label = c("Año"))
 issp99 <- rename_variables(issp99, v2 = "ID_SUBJECT")
 issp99$ID_SUBJECT <- sjlabelled::set_label(issp99$ID_SUBJECT, label = c("ID individuo"))
 
-# 3.3 COUNTRY ----
+# 3.3 FACTOR ----
+issp99 <- rename_variables(issp99, weight = "FACTOR")
+issp99$FACTOR <- sjlabelled::set_label(issp99$FACTOR, label = c("Factor expansion"))
+
+# 3.4 COUNTRY ----
 frq(issp99$v3)
 issp99 <- issp99 %>% mutate(COUNTRY = case_when(v3 == 1 ~ "Australia",
                                                 v3 == 2 | v3 == 3 ~ "Alemania",
                                                 v3 == 4 ~ "Gran Bretaña",
                                                 v3 == 5 ~ "Irlanda del Norte",
-                                                v3 == 6 ~ " USA",
+                                                v3 == 6 ~ "USA",
                                                 v3 == 7 ~ "Austria",
                                                 v3 == 8 ~ "Hungria",
                                                 v3 == 10 ~ "Irlanda",
@@ -90,18 +96,20 @@ issp99 <- issp99 %>% mutate(COUNTRY = case_when(v3 == 1 ~ "Australia",
                                                 TRUE ~ NA_character_))
 issp99$COUNTRY <- sjlabelled::set_label(issp99$COUNTRY, label = c("País"))
 
-# 3.4 SEX ----
+# 3.5 SEX ----
 frq(issp99$sex)
 issp99 <- issp99 %>% mutate(SEX = if_else(sex == 1, 'Hombre', 'Mujer', missing = NULL))
+issp99$SEX <- as.factor(issp99$SEX)
 issp99$SEX <- sjlabelled::set_label(issp99$SEX, label = c("Sexo"))
 
-# 3.5 UNION ----
+# 3.6 UNION ----
 frq(issp99$union)
 issp99$union <- car::recode(issp99$union, recodes = c("0 = NA; c(8,9) = NA"))
 issp99 <- issp99 %>% mutate(UNION = if_else(union ==  1, 'Si', 'No'))
+issp99$UNION <- as.factor(issp99$UNION)
 issp99$UNION <- sjlabelled::set_label(issp99$UNION, label = "Afiliación sindical")
 
-# 3.6 PARTY AFI ----
+# 3.7 PARTY AFI ----
 frq(issp99$x_prty)
 issp99 <- issp99 %>% mutate(PARTY_AFI = case_when(x_prty %in% c(101:106) ~ 'Si',
                                                   x_prty == 107 ~ 'No',
@@ -160,15 +168,126 @@ issp99 <- issp99 %>% mutate(PARTY_AFI = case_when(x_prty %in% c(101:106) ~ 'Si',
                                                   x_prty %in% c(3301:3316) ~ 'Si',
                                                   x_prty %in% c(3317:3318) ~ 'No',
                                                   TRUE ~ NA_character_))
+issp99$PARTY_AFI <- as.factor(issp99$PARTY_AFI)
 issp99$PARTY_AFI <- sjlabelled::set_label(issp99$PARTY_AFI, label = c("Afiliación partidaria"))
 
-# 3.7 INCOME ----
+# 3.8 INCOME ----
 sjmisc::descr(issp99$rincome)
 issp99$rincome <- car::recode(issp99$rincome, recodes = c("c(999997,999998,999999) = NA"))
-issp99 <- rename_variables(issp99, rincome = "INCOME") 
+issp99 <- rename_variables(issp99, rincome = "INCOME")
+issp99$INCOME <- as.numeric(issp99$INCOME)
 issp99$INCOME <- sjlabelled::set_label(issp99$INCOME, label = c("Ingreso"))
 
-# 3.8 PERCEIVED SOCIAL CONFLICT INDEX ----
+# 3.9 EDUCATION ----
+frq(issp99$educyrs) # We don't know if this is equal in every country; We use degree
+frq(issp99$degree)
+issp99 <- issp99 %>% mutate(EDUC = case_when(degree %in% c(1:6) ~ 'No',
+                                             degree == 7 ~ 'Si',
+                                             TRUE ~ NA_character_))
+issp99$EDUC <- as.factor(issp99$EDUC)
+issp99$EDUC <- sjlabelled::set_label(issp99$EDUC, label = c("Nivel educativo terciario completo"))
+table(issp99$degree, useNA = "ifany")
+table(issp99$EDUC, useNA = "ifany")
+
+# 3.10 CLASS ESCHEME E.O WRIGHT----
+
+## Employment relation
+frq(issp99$wrkgovt)
+issp99 <- issp99 %>% filter(wrkgovt != 0)
+
+## Salaried 
+issp99$wrkgovt <- as.numeric(issp99$wrkgovt)
+issp99 <- issp99 %>% mutate(prop_salaried1 = case_when(wrkgovt == 1 ~ 'Asalariado',
+                                                       wrkgovt == 2 ~ 'Asalariado',
+                                                       wrkgovt == 3 ~ 'Asalariado',
+                                                       wrkgovt == 6 ~ 'Asalariado',
+                                                       wrkgovt == 8 ~ 'Auto-empleado',
+                                                       wrkgovt == 9 ~ NA_character_))
+
+issp99 %>% count(wrkgovt) %>% mutate(prop = prop.table(n))
+issp99 %>% count(prop_salaried1) %>% mutate(prop = prop.table(n))
+
+## Owners
+#1. Pequeña burguesia: 0 a 1 empleados
+#2. Pequeños empleadores: de 2 a 9 empleados
+#3. Capitalistas: de 10 a más empleados
+
+frq(issp99$nemploy)
+issp99$owners <- as.numeric(issp99$nemploy)
+issp99 <- issp99 %>% mutate(owners = case_when(owners %in% c(0, 1, 9995, 9997, 9999) & prop_salaried1 == 'Auto-empleado' ~ '3.Pequeña burguesia',
+                                               owners %in% c(2:9) ~ '2.Pequeños empleadores',
+                                               owners %in% c(10: 9990) ~ '1.Capitalistas',
+                                               TRUE ~ NA_character_))
+
+issp99 %>% count(owners) %>% mutate(prop = prop.table(n))
+
+## Salaried final
+issp99 <- issp99 %>% mutate(salaried = if_else(is.na(owners), prop_salaried1, owners)) # if NA in owners, return the value in prop_salaried
+
+issp99 %>% count(prop_salaried1) %>% mutate(prop = prop.table(n))
+issp99 %>% count(salaried) %>% mutate(prop = prop.table(n))
+
+## Control
+issp99$wrksup <- as.numeric(issp99$wrksup)
+frq(issp99$wrksup)
+issp99 <- issp99 %>% mutate(control = case_when(wrksup == 1 ~ 'Control',
+                                                wrksup == 2 ~ 'No control',
+                                                wrksup %in% c(0, 7, 8, 9) ~ NA_character_))
+
+issp99$control <- as.factor(issp99$control)
+issp99 %>% count(control) %>% mutate(prop = prop.table(n))
+issp99 %>% count(wrksup) %>% mutate(prop = prop.table(n))
+
+## Skills
+issp99 <- rename_variables(issp99, isco88_4 = "ISCO88")
+frq(issp99$ISCO88)
+issp99 <- issp99 %>% filter(ISCO88!=0,ISCO88!=1,ISCO88!=2,ISCO88!=110) # eliminated FFAA and don't labor force people
+issp99$ISCO88 <- as.numeric(issp99$ISCO88)
+table(issp99$ISCO88)
+
+issp99$ISCO88 <- substr(issp99$ISCO88, start = 1, stop = 2) # 2 digits
+
+### Skills variable
+issp99 <- issp99 %>% mutate(skills = case_when(ISCO88 %in% c(1:25) ~ 'Expertos',
+                                                ISCO88 %in% c(30, 31, 32, 33, 34, 70, 71, 72, 73, 74, 75, 79) ~ 'Calificados',
+                                                ISCO88 %in% c(40, 41, 42, 51, 52, 61, 62, 80, 81, 82, 83, 84, 90, 91, 92, 93) ~ 'No calificados',
+                                                ISCO88 == 99 ~ NA_character_))
+
+issp99 %>% count(skills) %>% mutate(prop = prop.table(n))
+
+
+## Education control effect
+issp99 <- issp99 %>% mutate(skillsA = if_else(skills =="Expertos" & EDUC=="Si", "Expertos",
+                                         if_else(skills == "Expertos" & EDUC=="No", "Calificados",
+                                                 if_else(is.na(skills), NA_character_, "No calificados"))))
+
+
+issp99 %>% count(skills) %>% mutate(prop = prop.table(n))
+issp99 %>% count(skillsA) %>% mutate(prop = prop.table(n)) # Unskilled up
+
+## FINAL VARIABLE CLASS 
+
+issp99$CLASS <- NA
+issp99$CLASS <- with(issp99, ifelse(salaried=="1.Capitalistas", 1, CLASS))
+issp99$CLASS <- with(issp99, ifelse(salaried=="2.Pequeños empleadores", 2, CLASS))
+issp99$CLASS <- with(issp99, ifelse(salaried=="3.Pequeña burguesia", 3, CLASS))
+issp99$CLASS <- with(issp99, ifelse(salaried=="Asalariado" & control=="Control" & skillsA=="Expertos", 4, CLASS))
+issp99$CLASS <- with(issp99, ifelse(salaried=="Asalariado" & control=="No control" & skillsA=="Expertos", 5, CLASS))
+issp99$CLASS <- with(issp99, ifelse(salaried=="Asalariado" & control=="Control" & skillsA=="Calificados", 6, CLASS))
+issp99$CLASS <- with(issp99, ifelse(salaried=="Asalariado" & control=="Control" & skillsA=="No calificados", 7, CLASS))
+issp99$CLASS <- with(issp99, ifelse(salaried=="Asalariado" & control=="No control" & skillsA=="Calificados", 8, CLASS))
+issp99$CLASS <- with(issp99, ifelse(salaried=="Asalariado" & control=="No control" & skillsA=="No calificados", 9, CLASS))
+
+issp99$CLASS <- factor(issp99$CLASS,levels = c(1:9),
+                     labels = c("1.Capitalistas","2.Pequeños empleadores","3.Pequeña burguesia",
+                                "4.Expertos directivos","5.Expertos sin autoridad",
+                                "6.Supervisores calificados","7.Supervisores no calificados",
+                                "8.Trabajadores calificados","9.Trabajadores no calificados"))
+
+issp99 %>% filter(!is.na(CLASS)) %>% count(CLASS) %>% mutate(prop = prop.table(n)) 
+issp99$CLASS <- sjlabelled::set_label(issp99$CLASS, label = c("Posición de clase"))
+
+# 3.11 PERCEIVED SOCIAL CONFLICT INDEX ----
 ## Rich and poor 
 frq(issp99$v41)
 issp99 <- issp99 %>% mutate(CONFLICT_RP = case_when(v41 == 1 ~ 3,
@@ -216,17 +335,37 @@ issp99$CONFLICT_TB <- sjlabelled::set_label(issp99$CONFLICT_TB, label = c("Confl
 ## PSCI 
 issp99 <- issp99 %>% 
   rowwise() %>% 
-  mutate(PSCi = sum(CONFLICT_RP, CONFLICT_WCMC, CONFLICT_MW, CONFLICT_TB))
+  mutate(PSCi = sum(CONFLICT_RP, CONFLICT_WCMC, CONFLICT_MW, CONFLICT_TB, na.rm = F))
 
 issp99$PSCi <- sjlabelled::set_label(issp99$PSCi, label = c("Perceived Social Conflict Index"))
 frq(issp99$PSCi)
 sjPlot::plot_frq(na.omit(issp99$PSCi), type = "histogram", show.mean = TRUE) # Follows a normal distribution. Careful with the NA
 
 ## Cronbach's alpha
-
 matriz <- issp99 %>% select(CONFLICT_RP, CONFLICT_WCMC, CONFLICT_MW, CONFLICT_TB)
 psych::alpha(matriz) # coef = 0.81
 
 ## Polychoric alpha ordinal (Likert scale)
 matriz_poly <- polychoric(matriz) 
 psych::alpha(matriz_poly$rho) # coef = 0.85
+
+# View
+issp99 %>% 
+  filter(!is.na(PSCi)) %>% 
+  count(PSCi) %>% 
+  mutate(proporcion = prop.table(n))
+
+# 4. Level 2 data ----
+issp99 <- issp99 %>% select(YEAR,
+                            ID_SUBJECT,
+                            COUNTRY,
+                            FACTOR,
+                            SEX,
+                            INCOME,
+                            PARTY_AFI,
+                            UNION,
+                            CLASS,
+                            33:37)
+
+sapply(issp99, class)
+view(dfSummary(issp99, headings=FALSE, varnumbers = F, valid.col = T, na.col = T))
